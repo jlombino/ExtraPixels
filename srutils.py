a8f473c8290eb22e78377d873ab6b4241c91c021
Jason Lombino <jasonlombino@gmail.com>
2024-02-14 10:06:16 -0500

    Rework compare model function

--- srutils.py
+++ srutils.py
@@ -44,85 +44,51 @@ class ImageDataset(Dataset):
         
         return small_image, large_image
 
-def compare_model(generator, dataset_dir, device, index):
+def crop_images(image, locations, small):
+
+    if small:
+        locations = [location // 4 for location in locations]
+        size = 32
+    else:
+        size = 128
+        
+    crops = [image]
+    for location in locations:
+        crops.append(transforms.functional.crop(image,
+                                      top = location,
+                                      left = location,
+                                      height = size,
+                                      width = size))
+    crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
+               for image in crops]
+
+    return crops
+
+def compare_model(generator, dataset_dir, index):
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     fig, ax = plt.subplots(4, 4, figsize=(16,16))
 
-    for row in ax:
-        for image in row:
-            image.set_xticks([])
-            image.set_yticks([])
-    
     small, large = load_single_image(dataset_dir, index)
 
-    small_height = small.shape[1] 
-    small_width = small.shape[2] 
+    large_height = large.shape[1] 
+    large_width = large.shape[2] 
 
-    bilinear = transforms.Resize((small_height * 4, small_width * 4), 
+    bilinear = transforms.Resize((large_height, large_width), 
                                 antialias = True)(small)
     generated = generator(torch.unsqueeze(small,0).to(device)).to('cpu')
     generated = torch.squeeze(generated)
     generated = generated.detach()
 
     crop_locations = [400, 800, 1200]
-    crop_small_size = 32
-    crop_large_size = 128
 
-    small_crops = [
-        transforms.functional.crop(small,
-                                  top = loc//4,
-                                  left = loc//4,
-                                  height = crop_small_size,
-                                  width = crop_small_size)
-        for loc in crop_locations]
-    small_crops = [(torch.movedim(img,0,-1) * 128 + 128).to(torch.int)
-                   for img in small_crops]
-    
-    large_crops = [
-        transforms.functional.crop(large,
-                                  top = loc,
-                                  left = loc,
-                                  height = crop_large_size,
-                                  width = crop_large_size)
-        for loc in crop_locations]
-    large_crops = [(torch.movedim(img,0,-1) * 128 + 128).to(torch.int)
-                   for img in large_crops]
+    small_crops = crop_images(small, crop_locations, small = True)
+    bilinear_crops = crop_images(bilinear, crop_locations, small = False)
+    generated_crops = crop_images(generated, crop_locations, small = False)
+    large_crops = crop_images(large, crop_locations, small = False)
         
-    bilinear_crops = [
-        transforms.functional.crop(bilinear,
-                                  top = loc,
-                                  left = loc,
-                                  height = crop_large_size,
-                                  width = crop_large_size)
-        for loc in crop_locations]
-    bilinear_crops = [(torch.movedim(img,0,-1) * 128 + 128).to(torch.int)
-                   for img in bilinear_crops]
- 
-    generated_crops = [
-        transforms.functional.crop(generated,
-                                  top = loc,
-                                  left = loc,
-                                  height = crop_large_size,
-                                  width = crop_large_size)
-        for loc in crop_locations]
-    generated_crops = [(torch.movedim(img,0,-1) * 128 + 128).to(torch.int)
-                   for img in generated_crops]   
-    
-    small = (torch.movedim(small,0,-1) * 128 + 128).to(torch.int)
-    large = (torch.movedim(large,0,-1) * 128 + 128).to(torch.int)
-    bilinear = (torch.movedim(bilinear,0,-1) * 128 + 128).to(torch.int)
-    generated = (torch.movedim(generated,0,-1) * 128 + 128).to(torch.int)
-        
-    ax[0][0].imshow(small)
-    ax[0][0].set_title('Small')
-    ax[0][1].imshow(bilinear)
-    ax[0][1].set_title('Bilinear')
-    ax[0][2].imshow(generated)
-    ax[0][2].set_title('Neural Net')
-    ax[0][3].imshow(large)
-    ax[0][3].set_title('Original')
-
-    for row, axis in enumerate(ax[1:]):
+    for row, axis in enumerate(ax):
         axis[0].imshow(small_crops[row])
         axis[0].set_title('Small')
         axis[1].imshow(bilinear_crops[row])
@@ -131,6 +97,11 @@ def compare_model(generator, dataset_dir, device, index):
         axis[2].set_title('Neural Net')
         axis[3].imshow(large_crops[row])
         axis[3].set_title('Original')
+
+    for row in ax:
+        for axis in row:
+            axis.set_xticks([])
+            axis.set_yticks([])
     
     return fig
 
