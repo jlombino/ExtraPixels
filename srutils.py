45e4bb5a28440bbf65d52ab6b5aabf6acbd9db63
Jason Lombino <jasonlombino@gmail.com>
2024-02-21 12:04:47 -0500

    Add support for saving validation output images

--- srutils.py
+++ srutils.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import torch
 from torchvision import transforms
-from torchvision.io import read_image
+from torchvision.io import read_image, write_png
 from torch.utils.data import Dataset
 
 def load_single_image(directory, image_number):
@@ -44,14 +44,8 @@ class ImageDataset(Dataset):
         
         return small_image, large_image
 
-def crop_images(image, locations, small):
-
-    if small:
-        locations = [location // 4 for location in locations]
-        size = 32
-    else:
-        size = 128
-        
+def crop_images(image, locations, size):
+     
     crops = [image]
     for location in locations:
         crops.append(transforms.functional.crop(image,
@@ -59,16 +53,11 @@ def crop_images(image, locations, small):
                                       left = location,
                                       height = size,
                                       width = size))
-    crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
-               for image in crops]
-
     return crops
 
-def compare_model(generator, dataset_dir, index):
+def load_image_generate(generator, dataset_dir, index):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    fig, ax = plt.subplots(4, 4, figsize=(16,16))
 
     small, large = load_single_image(dataset_dir, index)
 
@@ -80,13 +69,38 @@ def compare_model(generator, dataset_dir, index):
     generated = generator(torch.unsqueeze(small,0).to(device)).to('cpu')
     generated = torch.squeeze(generated)
     generated = generated.detach()
+    
+    small = transforms.Resize((large_height, large_width), 
+                                antialias = False,
+                                interpolation = transforms.InterpolationMode.NEAREST)(small)
+    
+    return small, bilinear, generated, large
+
+def compare_model(generator, dataset_dir, index):
+
+    fig, ax = plt.subplots(4, 4, figsize=(16,16))
 
     crop_locations = [400, 800, 1200]
+    crop_size = 128
 
-    small_crops = crop_images(small, crop_locations, small = True)
-    bilinear_crops = crop_images(bilinear, crop_locations, small = False)
-    generated_crops = crop_images(generated, crop_locations, small = False)
-    large_crops = crop_images(large, crop_locations, small = False)
+    small, bilinear, generated, large = load_image_generate(generator, dataset_dir, index)
+
+    small_crops = crop_images(small, crop_locations, crop_size)
+    small_crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
+           for image in small_crops]
+    
+    bilinear_crops = crop_images(bilinear, crop_locations, crop_size)
+    bilinear_crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
+           for image in bilinear_crops]
+    
+    generated_crops = crop_images(generated, crop_locations, crop_size)
+    generated_crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
+           for image in generated_crops]
+    
+    large_crops = crop_images(large, crop_locations, crop_size)
+    large_crops = [(torch.movedim(image,0,-1) * 128 + 128).to(torch.int)
+           for image in large_crops]
+    
         
     for row, axis in enumerate(ax):
         axis[0].imshow(small_crops[row])
@@ -105,8 +119,37 @@ def compare_model(generator, dataset_dir, index):
     
     return fig
 
+def save_val_images(generator, dataset_dir, index, label, save_all):
 
+    crop_locations = [400, 1200]
+    crop_size = 128
 
+    small, bilinear, generated, large = load_image_generate(generator, dataset_dir, index)
 
+    small_crops = crop_images(small, crop_locations, crop_size)
+    small_crops = [(image * 128 + 128).to(torch.uint8)
+           for image in small_crops]
+    
+    bilinear_crops = crop_images(bilinear, crop_locations, crop_size)
+    bilinear_crops = [(image * 128 + 128).to(torch.uint8)
+           for image in bilinear_crops]
+    
+    generated_crops = crop_images(generated, crop_locations, crop_size)
+    generated_crops = [(image * 128 + 128).to(torch.uint8)
+           for image in generated_crops]
+    
+    large_crops = crop_images(large, crop_locations, crop_size)
+    large_crops = [(image * 128 + 128).to(torch.uint8)
+           for image in large_crops]
+    
+    for crop_idx, image in enumerate(generated_crops):
+        write_png(image, f'savedimages/{str(index)}/{label}_generated_part{str(crop_idx)}.png')
 
+    if save_all:
+        for crop_idx, image in enumerate(small_crops):
+            write_png(image, f'savedimages/{str(index)}/small_part{str(crop_idx)}.png')
+        for crop_idx, image in enumerate(bilinear_crops):
+            write_png(image, f'savedimages/{str(index)}/bilinear_part{str(crop_idx)}.png')
+        for crop_idx, image in enumerate(large_crops):
+            write_png(image, f'savedimages/{str(index)}/large_part{str(crop_idx)}.png')
 
